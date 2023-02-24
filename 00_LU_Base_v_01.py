a15860abdd72c202885d6a661c4b0fd996cde141
partschr1340 <125407721+partschr1340@users.noreply.github.com>
2023-02-24 13:18:01 +1300

    Add files via upload

--- 00_LU_Base_v_01.py
+++ 00_LU_Base_v_01.py
@@ -22,6 +22,26 @@ def instructions():
     print()
     return""
 
+def num_check(question,low,high):
+    error =  "Enter a number between 1 and 10 mf\n"
+
+    valid = False
+    while not valid:
+        try:
+            # ask the question
+            response = int(input(question))
+            # if the amount is too low/ too high to give
+            if low < response <= high:
+               return response
+
+            # output an error
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+
+
 #Main Routine goes here...
 played_before = yes_no("Have you played the game before?")
 
@@ -29,3 +49,8 @@ if played_before == "no":
    instructions()
 
 print("Programs Continues")
+
+#Ask user much they want to play with...
+how_much = num_check ("How much would you like to play with?", 0, 10)
+
+print("You will be spending ${}".format(how_much))
