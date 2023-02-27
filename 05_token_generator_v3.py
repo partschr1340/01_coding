bc91557b130ab95ec0da60fa88eb32b33eb86297
partschr1340 <125407721+partschr1340@users.noreply.github.com>
2023-02-28 10:13:19 +1300

    Add files via upload

---
+++ 05_token_generator_v3.py
@@ -0,0 +1,25 @@
+import random
+
+
+#main routine goes here
+
+tokens = ["unicorn","donkey","unicorn", "horse","horse", "zebra", "zebra","donkey", "donkey", "zebra"]
+STARTING_BALANCE=100
+
+balance = STARTING_BALANCE
+#Testing loop to generate 20 tokens
+for item in range (0,500):
+    chosen= random.choice(tokens)
+
+    #Adjust balance
+    if chosen== "unicorn":
+        balance += 4
+    elif chosen == "donkey":
+        balance -= 1
+    else:
+        balance -= 0.5
+
+print()
+print("Starting Balance: ${:.2f}".format(STARTING_BALANCE))
+print("Final Balance : ${:.2f}".format (balance))
+
